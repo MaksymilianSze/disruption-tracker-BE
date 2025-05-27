885c3ffb9123aa4f813f9804340d8aba711e6264
Maksymilian <71775083+MaksymilianSze@users.noreply.github.com>
2025-05-27 21:34:42 +0100

    Allow preview local url

--- src/server.js
+++ src/server.js
@@ -30,7 +30,7 @@ const PORT = process.env.PORT || 5000;
 app.use(
   cors({
     credentials: true,
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "http://localhost:4173"],
   })
 );
 app.use(express.json());
